ea450c1981797d58be29586363dac6786945e7e4
Rod Levy <rlevy@ncsa-mbp-04.ncsasports.org>
2013-09-19 10:35:30 -0500

    turning ttt into a gem

--- lib/ultimate_tic_tac_toe.rb
+++ lib/ultimate_tic_tac_toe.rb
@@ -1,7 +1,5 @@
 $: << File.expand_path(File.dirname(__FILE__) + "/ultimate_tic_tac_toe/")
 
-# puts $:
-
 require 'game'
 require 'board'
 require 'interface'
@@ -9,9 +7,5 @@ require 'computer'
 require "version"
 
 module UltimateTicTacToe
-HUMAN = "X"
-COMPUTER = "O"
 
-fun_game = Game.new(Interface.new, Computer.new)
-fun_game.play
 end
--- lib/ultimate_tic_tac_toe/version.rb
+++ lib/ultimate_tic_tac_toe/version.rb
@@ -1,3 +1,3 @@
 module UltimateTicTacToe
-  VERSION = "0.0.1"
+  VERSION = "0.0.21"
 end
